03d642f3ae4cb8b0729f47cab7f65072ecac2728
Michael Burka <contact@michaelburka.com>
2023-05-15 16:03:17 +0400

    .

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,5 +1,5 @@
 # birthday/views.py 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import BirthdayForm
 # Импортируем из utils.py функцию для подсчёта дней.
@@ -8,19 +8,18 @@ from .utils import calculate_birthday_countdown
 from .models import Birthday
 
 
-def birthday(request):
-    form = BirthdayForm(request.POST or None)
-    # Создаём словарь контекста сразу после инициализации формы.
+def birthday(request, pk=None):
+    if pk is not None:
+        instance = get_object_or_404(Birthday, pk=pk)
+    else:
+        instance = None
+    form = BirthdayForm(request.POST or None, instance=instance)
     context = {'form': form}
-    # Если форма валидна...
     if form.is_valid():
         form.save()
-        # ...вызовем функцию подсчёта дней:
         birthday_countdown = calculate_birthday_countdown(
-            # ...и передаём в неё дату из словаря cleaned_data.
             form.cleaned_data['birthday']
         )
-        # Обновляем словарь контекста: добавляем в него новый элемент.
         context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
 
@@ -32,3 +31,12 @@ def birthday_list(request):
     context = {'birthdays': birthdays}
     return render(request, 'birthday/birthday_list.html', context)
 
+
+def delete_birthday(request, pk):
+    instance = get_object_or_404(Birthday, pk=pk)
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    if request.method == 'POST':
+        instance.delete()
+        return redirect('birthday:list')
+    return render(request, 'birthday/birthday.html', context)
