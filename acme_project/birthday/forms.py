0e5672d6fb94ca12dc055bffcb3e27c6192773a8
Michael Burka <contact@michaelburka.com>
2023-05-20 23:42:30 +0400

    .

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,9 +1,11 @@
 # birthday/forms.py
 from django import forms
-
+from django.core.exceptions import ValidationError
 # Импортируем класс модели Birthday.
 from .models import Birthday
 
+
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 # Для использования формы с моделями меняем класс на forms.ModelForm.
 class BirthdayForm(forms.ModelForm):
@@ -16,5 +18,15 @@ class BirthdayForm(forms.ModelForm):
         # Указываем, что надо отобразить все поля.
         fields = '__all__'
         widgets = {
-            'birthday': forms.DateInput(attrs={'type': 'date'})
+            'birthday': forms.DateInput(format='%d/%m/%Y', attrs={'type': 'date'})
         }
+        
+    def clean(self):
+        # Вызов родительского метода clean.
+        super().clean()
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
